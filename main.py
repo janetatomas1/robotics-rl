070e951bd4ca75826774cc45c2123c82a3fce3c3
janetatomas1 <janetatomas11@gmail.com>
2022-10-12 01:24:39 +0200

    Structure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gym
 import numpy as np
 
-from stable_baselines3 import DDPG
+from stable_baselines3 import DDPG, TD3, PPO
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 from os import path
 
@@ -16,9 +16,4 @@ model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
 model.learn(total_timesteps=10000, log_interval=10)
 env = model.get_env()
 
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
 
