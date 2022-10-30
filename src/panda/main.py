c20a8d8b4643f30b416cafde7441a6a00910be66
janetatomas1 <janetatomas11@gmail.com>
2022-10-30 14:52:08 +0100

    structure

--- src/panda/main.py
+++ src/panda/main.py
@@ -3,7 +3,7 @@ from stable_baselines3.td3 import TD3, policies
 
 from env import PandaEnv
 
-scene = pathlib.Path(pathlib.Path(__file__).parent.parent, 'scenes', 'scene_panda.ttt')
+scene = pathlib.Path(pathlib.Path(__file__).parent.parent.parent, 'scenes', 'scene_panda.ttt')
 
 env = PandaEnv(scene=str(scene))
 
