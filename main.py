b1e6fc07ea577c4985983029b96e03bf52153fc6
janetatomas1 <janetatomas11@gmail.com>
2022-11-06 01:16:50 +0100

    Typo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     client = docker.DockerClient(version='auto')
 
     home_dir = os.environ["HOME"]
-    volume_host_path_prefix = "{}/resuls".format(home_dir)
+    volume_host_path_prefix = "{}/results".format(home_dir)
     container_name = ''.join(random.sample(string.ascii_lowercase + string.digits, 20))
 
     volume_host_path = "{}/{}".format(volume_host_path_prefix, container_name)
