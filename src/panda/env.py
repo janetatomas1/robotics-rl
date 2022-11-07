4aefa97d013a35f4b954da7e4ea716d794305760
janetatomas1 <janetatomas11@gmail.com>
2022-11-07 23:45:10 +0100

    Reward parametrized

--- src/panda/env.py
+++ src/panda/env.py
@@ -4,12 +4,48 @@ from pyrep import PyRep
 from pyrep.robots.arms.panda import Panda
 from pyrep.objects.shape import Shape
 import numpy as np
-import torch
+import math
+
+
+class Constants:
+    a = math.pow(2, -10)
+    b = -10
+
+
+def sparse_reward(**kwargs):
+    done = kwargs["done"]
+    close = kwargs["close"]
+
+    if close:
+        return 1
+    elif not done:
+        return -1
+
+    return -2
+
+
+def f(x):
+    if x < Constants.a:
+        return Constants.b
+
+    return math.log2(x)
+
+
+def shaped_reward(**kwargs):
+    env = kwargs["env"]
+    state = env._get_state()
+    current_pos = env.robot.get_tip().get_position()
+    target_pos = state[:3]
+    joint_values = state[3:]
+    joint_low = env.low
+    joint_high = env.high
+    minimum = np.concatenate([joint_values - joint_low, joint_high - joint_values]).min()
+    return f(minimum) + np.linalg.norm(current_pos - target_pos)
 
 
 class PandaEnv(Env):
     def __init__(self, scene, threshold=0.3, joints=None, episode_length=100,
-                 headless=False, reset_actions=10, log_dir=None, logger_class=None):
+                 headless=False, reset_actions=10, log_dir=None, logger_class=None, reward_fn=sparse_reward):
         self.robot = None
         self.target = None
         self.reset_actions = reset_actions
@@ -17,8 +53,9 @@ class PandaEnv(Env):
         self.headless = headless
         self.log_dir = log_dir
         self.episode_length = episode_length
-        self.steps = 0
         self.scene = scene
+        self.reward = reward_fn
+        self.steps = 0
         self.pyrep = PyRep()
 
         self.pyrep.launch(scene_file=self.scene, headless=headless)
@@ -27,12 +64,12 @@ class PandaEnv(Env):
             else [i for i in range(len(self.robot.joints))]
 
         _, joint_intervals = self.robot.get_joint_intervals()
-        low = [joint_intervals[j][0] for j in self.joints]
-        high = [joint_intervals[j][1] for j in self.joints]
+        self.low = np.array([joint_intervals[j][0] for j in self.joints])
+        self.high = np.array([joint_intervals[j][1] for j in self.joints])
 
         self.observation_space = spaces.Box(
-            low=np.array([0.8, -0.2, 0.5] + low),
-            high=np.array([1.0, 0.2, 1.4] + high),
+            low=np.concatenate([[0.8, -0.2, 0.5], self.low]),
+            high=np.concatenate([[1.0, 0.2, 1.4], self.high]),
             dtype=np.float64,
         )
         self.action_space = spaces.Box(
@@ -86,14 +123,6 @@ class PandaEnv(Env):
         return bool(np.linalg.norm(self.target.get_position() - self.robot.get_tip().get_position())
                     <= self.threshold)
 
-    def _reward(self, done=False, close=False):
-        if close:
-            return 100
-        elif not done:
-            return -np.linalg.norm(self.target.get_position() - self.robot.get_tip().get_position())
-
-        return -100
-
     def _is_done(self):
         return self._is_close() or self.steps >= self.episode_length
 
@@ -103,15 +132,17 @@ class PandaEnv(Env):
     def step(self, action):
         self.steps += 1
 
-        next_state = self._get_state()[3:] + self.timestep * action
-        if self.observation_space.contains(next_state):
-            return self._get_state(), -100, False, self._info()
-
         self.move(action)
 
         done = self._is_done()
         close = self._is_close()
-        reward = self._reward(done, close)
+
+        reward_kwargs = {
+            "env": self,
+            "done": done,
+            "close": close,
+        }
+        reward = self.reward(**reward_kwargs)
 
         if self.logger is not None:
             self.logger.step(reward, done, close)
