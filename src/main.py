2460763a959326a952937e1ab85c2074b0d11b7d
janetatomas1 <janetatomas11@gmail.com>
2022-11-10 14:04:40 +0100

    Template

--- src/main.py
+++ src/main.py
@@ -2,67 +2,12 @@
 import sys
 import os
 import importlib
-from logger import Logger
-
-joints = [0, 1, 2]
-
-
-def main():
-    stable_baselines3 = importlib.import_module("stable_baselines3")
-    noise_class = stable_baselines3.noise.NormalActionNoise
-
-    np = importlib.import_module("numpy")
-
-    n_actions = len(joints)
-    action_noise = noise_class(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-
-    torch = importlib.import_module("torch")
-    nn = torch.nn
-    activation_fn = nn.Tanh
-
-    torch.set_num_threads(2)
-
-    env_module = importlib.import_module("panda.env")
-    env_reward_fn = env_module.shape_reward
-    env_class = env_module.PandaEnv
-    env_kwargs = {
-        "scene": "/opt/robotics-rl/scenes/scene_panda.ttt",
-        "headless": True,
-        "joints": joints,
-        "episode_length": 50,
-        "log_dir": "/opt/results",
-        "logger_class": Logger,
-        "reward_fn": env_reward_fn,
-    }
-    env = env_class(**env_kwargs)
-
-    callback_class = importlib.import_module("panda.callback").CustomCallback
-    callback_kwargs = {
-        "n_steps": 100000,
-        "save_path": "/opt/results/models"
-    }
-    callback = callback_class(**callback_kwargs)
-
-    algorithm_class = stable_baselines3.TD3
-    algorithm_kwargs = {
-        "policy": "MlpPolicy",
-        "policy_kwargs": {
-            "net_arch": [100, 100],
-            "activation_fn": activation_fn,
-        },
-        "action_noise": action_noise,
-    }
-
-    learn_kwargs = {
-        "total_timesteps": 10000000
-    }
-
-    algorithm = algorithm_class(env=env, action_noise=action_noise, **algorithm_kwargs)
-    algorithm.learn(**learn_kwargs, callback=callback)
 
 
 if __name__ == "__main__":
     venv_path = os.environ["VENV"]
     sys.path.insert(0, venv_path)
+
+    main = importlib.import_module("panda.train").main
 
     main()
---
+++ src/panda/train.py
@@ -0,0 +1,60 @@
+
+from stable_baselines3 import TD3
+from stable_baselines3.common.noise import NormalActionNoise
+import numpy as np
+import torch
+import torch.nn as nn
+import pathlib
+
+from src.logger import Logger
+from src.callback import CustomCallback
+from .env import PandaEnv, sparse_reward
+
+
+def main():
+    joints = [0, 1, 2, 3, 4, 5, 6]
+    n_actions = len(joints)
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+
+    torch.set_num_threads(2)
+
+    scene = pathlib.Path(pathlib.Path(__file__).parent.parent.parent, 'scenes', 'scene_panda.ttt')
+
+    env_kwargs = {
+        "scene": str(scene),
+        "headless": True,
+        "joints": joints,
+        "episode_length": 50,
+        "log_dir": "/opt/results",
+        "logger_class": Logger,
+        "reward_fn": sparse_reward
+    }
+    env = PandaEnv(**env_kwargs)
+
+    callback_kwargs = {
+        "n_steps": 100000,
+        "save_path": "/opt/results/models"
+    }
+    callback = CustomCallback(**callback_kwargs)
+
+    algorithm_kwargs = {
+        "env": env,
+        "policy": "MlpPolicy",
+        "policy_kwargs": {
+            "net_arch": [100, 100],
+            "activation_fn": nn.Tanh,
+        },
+        "action_noise": action_noise,
+    }
+
+    learn_kwargs = {
+        "callback": callback,
+        "total_timesteps": 10000000
+    }
+
+    algorithm = TD3(**algorithm_kwargs)
+    algorithm.learn(**learn_kwargs)
+
+
+if __name__ == "__main__":
+    main()
