ad94c969ceaf6b0ff2a36efaf22a0c22ef36ba4a
janetatomas1 <janetatomas11@gmail.com>
2023-02-18 19:14:13 +0100

    Scripts

--- scripts/setup-nico.sh
+++ scripts/setup-nico.sh
@@ -3,9 +3,7 @@
 
 virtualenv --python=python3.7 ~/.NICO-python3
 
-source ~/.NICO-python3/bin/activate
-
-pip install "setuptools==57.5.0"
+/root/.NICO-python3/bin/pip install "setuptools==57.5.0"
 
 source NICO-software/api/NICO-python3.bash
 
