44800567ac05da85c3c403e2093214351867735a
janetatomas1 <janetatomas11@gmail.com>
2023-02-18 19:45:45 +0100

    Scripts

--- scripts/setup-nico.sh
+++ scripts/setup-nico.sh
@@ -7,4 +7,4 @@ virtualenv --python=python3.7 ~/.NICO-python3
 
 source NICO-software/api/NICO-python3.bash
 
-/root/.NICO-python3/bin/pip install -r dev.txt
+/root/.NICO-python3/bin/pip install -r nico-dev.txt
